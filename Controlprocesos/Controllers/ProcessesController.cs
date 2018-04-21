d22a3da449f64d7384f0feea52a5f176fa320732
Manuel Ureña <manuelcr1990@gmail.com>
2018-04-20 21:42:44 -0600

    agragado processes

---
+++ Controlprocesos/Controllers/ProcessesController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Controlprocesos.Model;
+
+namespace Controlprocesos.Controllers
+{
+    public class ProcessesController : Controller
+    {
+        private CP db = new CP();
+
+        // GET: Processes
+        [Authorize(Roles = "Manager,Employee")]
+        public ActionResult Index()
+        {
+            return View(db.Processes.ToList());
+        }
+
+        // GET: Processes/Details/5
+        [Authorize(Roles = "Manager,Employee")]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Process process = db.Processes.Find(id);
+            if (process == null)
+            {
+                return HttpNotFound();
+            }
+            return View(process);
+        }
+
+        // GET: Processes/Create
+        [Authorize(Roles = "Manager")]
+        public ActionResult Create() 
+        {
+            return View();
+        }
+
+        // POST: Processes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Manager")]
+        public ActionResult Create([Bind(Include = "Id,Title,Description,Notes")] Process process)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Processes.Add(process);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(process);
+        }
+
+        // GET: Processes/Edit/5
+        [Authorize(Roles = "Manager")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Process process = db.Processes.Find(id);
+            if (process == null)
+            {
+                return HttpNotFound();
+            }
+            return View(process);
+        }
+
+        // POST: Processes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Manager")]
+        public ActionResult Edit([Bind(Include = "Id,Title,Description,Notes")] Process process)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(process).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(process);
+        }
+
+        // GET: Processes/Delete/5
+        [Authorize(Roles = "Manager")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Process process = db.Processes.Find(id);
+            if (process == null)
+            {
+                return HttpNotFound();
+            }
+            return View(process);
+        }
+
+        // POST: Processes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Manager")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Process process = db.Processes.Find(id);
+            db.Processes.Remove(process);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
