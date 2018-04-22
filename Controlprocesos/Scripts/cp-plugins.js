dbc23076dafc20f616e5a711cdc4af2b36d71c7f
Kevin Rodriguez <darkndream@hotmail.com>
2018-04-21 18:49:00 -0600

    Adding changes

---
+++ Controlprocesos/Scripts/cp-plugins.js
@@ -0,0 +1,7 @@
+﻿var CPPlugins = (function () {
+    return {
+        InitDateTimePickers: function () {
+            $('.bs3DateTimePicker').datetimepicker({locale: 'es', format: 'yyyy-MM-dd HH:mm:ss'})
+        }
+    }
+}())
